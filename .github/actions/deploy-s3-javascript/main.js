9931a53d32485e27a11504e51445ea16e369b98b
argally <amandargalligan@gmail.com>
2026-01-16 17:44:04 +0100

    copy files to S3 bucket

--- .github/actions/deploy-s3-javascript/main.js
+++ .github/actions/deploy-s3-javascript/main.js
@@ -6,13 +6,13 @@ function run() {
     // Get some input values 
     const bucket = core.getInput('bucket', {required: true});
     const bucketRegion = core.getInput('bucket-region', {required:true}); // setting this to true even though this isn't a mandatory input in the action it is needed for the code to run 
-    //const distFolder = core.getInput('dist-folder', {required: true});
+    const distFolder = core.getInput('dist-folder', {required: true});
 
     //Upload the files to our bucket 
     const S3Uri = `s3://${bucket}`;
     core.notice(`S3Uri: ${S3Uri}`);
     core.notice('Hello from my custom JS action');
-    exec.exec(`aws s3 ls ${S3Uri} --region ${bucketRegion}`);
+    exec.exec(`aws s3 sync ${distFolder} ${S3Uri} --region ${bucketRegion}`);
 
 }
 
